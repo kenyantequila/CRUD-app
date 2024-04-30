6fe2537e8a6ba0b4b3fba6809ec63dee7ea6f3bb
kenyantequila <abdishakur431@gmail.com>
2024-04-30 21:56:47 +0300

    first commit

---
+++ crudapp/src/Components/Itemcomponents.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Itemcomponents() {
+  return (
+    <div></div>
+  )
+}
+
+export default Itemcomponents
---
+++ crudapp/src/Components/Listcomponents.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Listcomponents.css'
+
+function Listcomponents({props}) {
+  return props.map(({id,first_name,last_name,email,gender,fee_balance})=> (
+    <div key={id} className='props'>
+      <h4>first_name:{first_name}</h4>
+      <h4>last_name:{last_name}</h4>
+      <p>email:{email}</p>
+      <p>gender:{gender}</p>
+      <p>fee_balance:{fee_balance}</p> <br/>
+      <button >Delete</button> 
+
+    </div>
+  ))}
+
+
+export default Listcomponents
